9e682e78e27b44281ec0b5a9eb97b55243a25964
Ben Garcia <ben@seebenprogram.com>
2018-02-02 22:33:53 -0500

    Brainstorm js element id's required

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+/*
+
+html element id's:
+
+js-game-alert
+
+js-chosen-character
+js-chosen-enemy
+
+js-character-1
+js-character-2
+js-character-3
+js-character-4
+
+js-enemy-1
+js-enemy-2
+js-enemy-3
+js-enemy-4
+
+js-attack-button
+
+js-reset-button
+
+
+*/
