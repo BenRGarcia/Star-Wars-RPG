73a2ea6242703b922602496bc84a3141c18502dd
Ben Garcia <ben@seebenprogram.com>
2018-02-04 21:52:08 -0500

    Add jQuery to render chosen character to DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,8 +12,8 @@ const availableCharacters = {
       _baseAttackPower: 6,
       _attackPower: 6,
       _counterAttackPower: 10,
-      _imgSrc: "../images/boba.jpg",
-      _imgArenaSrc: "../images/fight-boba.jpg",
+      _imgSrc: "./assets/images/boba.jpg",
+      _imgArenaSrc: "./assets/images/fight-boba.jpg",
       _imgAlt: "Picture of Boba Fett"
     },
     {
@@ -22,8 +22,8 @@ const availableCharacters = {
       _baseAttackPower: 6,
       _attackPower: 6,
       _counterAttackPower: 15,
-      _imgSrc: "../images/darth.jpg",
-      _imgArenaSrc: "../images/fight-darth.png",
+      _imgSrc: "./assets/images/darth.jpg",
+      _imgArenaSrc: "./assets/images/fight-darth.png",
       _imgAlt: "Picture of Darth Maul"
     },
     {
@@ -32,8 +32,8 @@ const availableCharacters = {
       _baseAttackPower: 6,
       _attackPower: 6,
       _counterAttackPower: 20,
-      _imgSrc: "../images/hans.jpg",
-      _imgArenaSrc: "../images/fight-hans.jpg",
+      _imgSrc: "./assets/images/hans.jpg",
+      _imgArenaSrc: "./assets/images/fight-hans.jpg",
       _imgAlt: "Picture of Hans Solo"
     },
     {
@@ -42,8 +42,8 @@ const availableCharacters = {
       _baseAttackPower: 6,
       _attackPower: 6,
       _counterAttackPower: 25,
-      _imgSrc: "../images/yoda.jpg",
-      _imgArenaSrc: "../images/fight-yoda.jpg",
+      _imgSrc: "./assets/images/yoda.jpg",
+      _imgArenaSrc: "./assets/images/fight-yoda.jpg",
       _imgAlt: "Picture of Yoda"
     }
   ],
@@ -55,23 +55,25 @@ const availableCharacters = {
     // Method call example: availableCharacters.getCharacter("Yoda")
     for (let i = 0; i < this._characterArray.length; i++) {
       if (this._characterArray[i]._name === name) {
+        let hero = this._characterArray[i]
         this.addToUnavailable(i);
         this.deleteCharacter(i);
-        return this._characterArray[i];
+        return hero;
       }
     }
   },
 
   // Add chosen character to _unavailable array
   addToUnavailable(index) {
-    this._unavailable.push(this._characterArray[i])
-  }
+    this._unavailable.push(this._characterArray[index])
+  },
 
   // Remove character from available game play
   deleteCharacter(index) {
     this._characterArray.splice(index, 1);
   },
 
+  // Put all characters from _unavailable back into _characterArray
   resetCharacters() {
     for (let i = 0; i < this._unavailable.length; i++) {
       this._characterArray.push(this._unavailable[i]);
@@ -118,6 +120,14 @@ const gameProps = {
 
   get enemyName() {
     return this._enemy._name;
+  },
+
+  get heroImgSrc() {
+    return this._hero._imgArenaSrc;
+  },
+
+  get heroImgAlt() {
+    return this._hero._imgAlt;
   },
 
   get heroHP() {
@@ -181,94 +191,167 @@ const gameProps = {
  *  View -  output representation of information
  */
 
+const DOM = {
+  render(stageNumber) {
+
+    switch (stageNumber) {
+      case '0':
+        // New Round
+        break;
+      case '1':
+        // After player clicks to choose character, chosen character zaps to arena area
+          // render image to js-chosen-character (of chosen character) 
+          $('#js-chosen-character').attr("src", gameProps.heroImgSrc);
+          $('#js-chosen-character').attr("alt", gameProps.heroImgAlt);
+          // render update to 'choose character' area (will now be empty)
+          $('#js-character-section').detach();
+          // render update to 'choose enemy' area (will be filled with remaining characters)
+        break;
+      case '2':
+        // After player clicks to choose enemy
+          // render update to js-chosen enemy (of chosen enemy)
+          // render update to 'choose' enemy area (will remove the enemy just chosen)
+        break;
+      case '3':
+        // During battle
+          // Update js-game-alert1 & js-game-alert2 area with damage stats
+        break;
+      case '4':
+        //
+        break;
+      case '5':
+        //
+        break;
+      case '6':
+        //
+        break;
+      case '7':
+        //
+        break;
+      case '8':
+        //
+        break;
+      case '9':
+        //
+        break;
+      default:
+        console.log(`Stage ${stage} failed to render!`);
+        break;
+
+    }
+  }
+};
+
 /*
  * html element id's:
- *     js-game-alert
  *     js-chosen-character
  *     js-chosen-enemy
- *     js-character-1
- *     js-character-2
- *     js-character-3
- *     js-character-4
- *     js-enemy-1
- *     js-enemy-2
- *     js-enemy-3
- *     js-attack-reset-button
+ *     js-game-alert
+ *     js-hero-hp
+ *     js-enemy-hp
  */
 
 /*
  *  Controller - accepts input and converts it to commands for the model or view
  */
 
- //const arena(name, role) {
+const gameEngine = {
 
-  // 'role' is either 'character' or 'enemy'
-    // gameCharacters.chooseCharacter() or gameCharacters.chooseEnemy()
+  // Toggle invalid user input 
+  _gameInProgress: false,
+
+  chosenCharacter(name) {
+
+    this._gameInProgress = true;
+
+    // Pull character from object bank
+    let hero = availableCharacters.getCharacter(name);
+
+    // Send to gameProps, set as Hero
+    gameProps.hero = hero;
+
+    // updates to DOM
+    DOM.render('1');
+
+  },
+
+  chosenEnemy() {},
+
+  // After each 'attack'...
+
+  // First, test if user is defeated
+      // if yes, Alert to loss, reset game
+
+  // Second, test if enemy is defeated
+      // Alert to enemy defeat
+      // Test if enemies remain, 
+        // if not, alert to user won game, reset game
+        // if so, player to choose next enemy
+
+  reset() {
+    this._gameInProgress = false;
+  }
+};
 
   // Battling: 1) enemy sustains damage, 2) character sustains damage
     // 1) gameCharacters.currentEnemy.damageSustained(currentCharacter.attackPower)
     //    gameCharacters.currentCharacter.increaseAttackPower();
     // 2) gameCharacters.currentCharacter.damageSustained(gameCharacters.currentEnemy.counterAttackPower);
 
-  // After each 'attack'...
-
-    // First, test if user is defeated
-      // if yes, Alert to loss, reset game
-
-    // Second, test if enemy is defeated
-      // Alert to enemy defeat
-      // Test if enemies remain, 
-        // if not, alert to user won game, reset game
-        // if so, player to choose next enemy
- //};
 
 // Shorthand for $( document ).ready()
 $(function() {
 
-  // User clicks chosen character
-  $('#js-character-1').on('click', () => {
-    console.log(`character1 was just clicked!`);
-    arena.func("character1", "chosenCharacter");
-  });
+  // No clicks here until game is over/reset
+  if (!gameEngine._gameInProgress) {
 
-  $('#js-character-2').on('click', () => {
-    console.log(`character2 was just clicked!`);
-    arena.func("character2", "chosenCharacter");
-  });
+    // User clicks chosen character
+    $('#js-boba').on('click', () => {
+      console.log(`character1 was just clicked!`);
+      gameEngine.chosenCharacter("Boba Fett");
+    });
 
-  $('#js-character-3').on('click', () => {
-    console.log(`character3 was just clicked!`);
-    arena.func("character3", "chosenCharacter");
-  });
+    $('#js-darth').on('click', () => {
+      console.log(`character2 was just clicked!`);
+      gameEngine.chosenCharacter("Darth Maul");
+    });
 
-  $('#js-character-4').on('click', () => {
-    console.log(`character4 was just clicked!`);
-    arena.func("character4", "chosenCharacter");
-  });
+    $('#js-hans').on('click', () => {
+      console.log(`character3 was just clicked!`);
+      gameEngine.chosenCharacter("Hans Solo");
+    });
+
+    $('#js-yoda').on('click', () => {
+      console.log(`character4 was just clicked!`);
+      gameEngine.chosenCharacter("Yoda");
+    });
+  }
 
   // User chooses enemy to fight
   $('#js-enemy-1').on('click', () => {
-    console.log(`character1 was just chosen as the enemy`);
-    arena.func("character1", "chosenEnemy");
+    console.log(`enemy1 was just chosen as the enemy`);
+    // set as enemy
   });
 
   $('#js-enemy-2').on('click', () => {
-    console.log(`character2 was just chosen as the enemy`);
-    arena.func("character2", "chosenEnemy");
+    console.log(`enemy2 was just chosen as the enemy`);
+    // set as enemy
   });
 
   $('#js-enemy-3').on('click', () => {
-    console.log(`character3 was just chosen as the enemy`);
-    arena.func("character3", "chosenEnemy");
+    console.log(`enemy3 was just chosen as the enemy`);
+    // set as enemy
   });
 
   // User clicks 'attack' button
   $().on('click', () => {
-
+    console.log(`The 'attack' button was clicked`);
+    // call attack/counter attack on character/current enemy
   });
 
   // User clicks 'reset' button, only visible after loss or win
   $().on('click', () => {
-    
+    console.log(`The 'reset' button was clicked`);
+    // reset game props
   });
 });
